5c1314393e63963da92599e5cf6449b2ca137be1
santhoshReddyMekala <santhosh.4a5@gmail.com>
2014-07-24 22:15:12 -0400

    santhosh
    santhosh

---
+++ SimpleTablePropertyList/SimpleTable/SimpleTableViewController.m
@@ -0,0 +1,123 @@
+//
+//  SimpleTableViewController.m
+//  SimpleTable
+//
+//  Created by Simon Ng on 16/4/12.
+//  Copyright (c) 2012 AppCoda. All rights reserved.
+//
+
+#import "SimpleTableViewController.h"
+#import "SimpleTableCell.h"
+
+@interface SimpleTableViewController ()
+
+@end
+
+@implementation SimpleTableViewController
+{
+    NSArray *tableData;
+    NSArray *thumbnails;
+    NSArray *prepTime;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Find out the path of recipes.plist
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"recipes" ofType:@"plist"];    
+
+    // Load the file content and read the data into arrays
+    NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:path];
+    tableData = [dict objectForKey:@"RecipeName"];
+    thumbnails = [dict objectForKey:@"Thumbnail"];
+    prepTime = [dict objectForKey:@"PrepTime"];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    
+//    NSString *testPlistPath = [[NSBundle mainBundle]pathForResource:@"test" ofType:@"plist"];
+//    if (![[NSFileManager defaultManager] fileExistsAtPath:testPlistPath]) {
+//        [[NSFileManager defaultManager] createFileAtPath:testPlistPath contents:nil attributes:nil];
+//    }
+//
+    
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *testPlistPath = [documentsDirectory stringByAppendingPathComponent:@"/test.plist"];
+
+    
+    NSDictionary *testDict = [NSDictionary dictionaryWithObjectsAndKeys:@"Jenny",@"name1",@"James",@"name2",@"Justin",@"name3", nil];
+    [testDict writeToFile:testPlistPath atomically:YES];
+    
+}
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 78;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+
+    SimpleTableCell *cell = (SimpleTableCell *)[tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil)
+    {
+        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"SimpleTableCell" owner:self options:nil];
+        cell = [nib objectAtIndex:0];
+    } 
+    
+    cell.nameLabel.text = [tableData objectAtIndex:indexPath.row];
+    cell.thumbnailImageView.image = [UIImage imageNamed:[thumbnails objectAtIndex:indexPath.row]];
+    cell.prepTimeLabel.text = [prepTime objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"didSelectRowAtIndexPath");
+    /*UIAlertView *messageAlert = [[UIAlertView alloc]
+                                    initWithTitle:@"Row Selected" message:@"You've selected a row" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];*/
+    UIAlertView *messageAlert = [[UIAlertView alloc]
+                                 initWithTitle:@"Row Selected" message:[tableData objectAtIndex:indexPath.row] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    // Display the Hello World Message
+    [messageAlert show];
+    
+    // Checked the selected row
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"willSelectRowAtIndexPath");
+    if (indexPath.row == 0) {
+        return nil;
+    }
+    
+    return indexPath;
+}
+
+@end
+
